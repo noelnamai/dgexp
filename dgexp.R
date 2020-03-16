6d71d245f21375af20ede944f62750d21bcb4486
noelnamai <noelnamai@yahoo.com>
2020-03-16 18:19:13 -0400

    update

--- dgexp.R
+++ dgexp.R
@@ -1,29 +1,32 @@
 
-setwd("C:/Users/noel.namai/personal/asimov")
+
+setwd("C:/Users/noel.namai/personal/asimov/results/")
 
 library("DESeq2")
 
-sample.files = grep("state", list.files("./results"), value = TRUE)
+sample.files = grep("state", list.files("."), value = TRUE)
 file.names   = sub("\\.tsv$", "", sample.files)
 sample.table = as.data.frame(matrix(unlist(strsplit(file.names, "_")), ncol = 2, byrow = TRUE))
 sample.table = cbind(file.names, sample.files, sample.table)
 colnames(sample.table) = c("samplename", "filename", "state", "replicate")
 
 ddshtseq = DESeqDataSetFromHTSeqCount(sampleTable = sample.table,
-                                      directory = "./results",
+                                      directory = ".",
                                       design = ~ state)
 dds = DESeq(ddshtseq)
 res = results(dds, lfcThreshold = log2(1.5), alpha = 0.1)
 dexp.genes = as.data.frame(res[!is.na(res$padj) &
                                  res$padj < 0.1 &
-                                 res$log2FoldChange > log2(1.5),])
+                                 res$log2FoldChange > log2(1.5), ])
 
-write.table(x = as.data.frame(res),
-            file = "./results/genes_results.tsv", 
-            sep = "\t",
-            na = "0")
+write.table(
+  x = as.data.frame(res),
+  file = "genes-results.tsv",
+  sep = "\t",
+  na = "0"
+)
 
 write.table(x = dexp.genes,
-            file = "./results/dexp_genes.tsv", 
+            file = "dexp-genes.tsv",
             sep = "\t",
             na = "0")
