d7b043dd3ce192790c1026516c2d0c52ae558b8b
noelnamai <noelnamai@yahoo.com>
2020-03-17 05:27:23 -0400

    update

--- dgexp.R
+++ dgexp.R
@@ -1,13 +1,19 @@
 
 setwd("C:/Users/noel.namai/personal/asimov/results/")
 
+########################################################################################################
+
 library("DESeq2")
+
+########################################################################################################
 
 sample.files = grep("state", list.files("."), value = TRUE)
 file.names   = sub("\\.tsv$", "", sample.files)
 sample.table = as.data.frame(matrix(unlist(strsplit(file.names, "_")), ncol = 2, byrow = TRUE))
 sample.table = cbind(file.names, sample.files, sample.table)
 colnames(sample.table) = c("samplename", "filename", "state", "replicate")
+
+########################################################################################################
 
 ddshtseq = DESeqDataSetFromHTSeqCount(sampleTable = sample.table,
                                       directory = ".",
@@ -18,6 +24,8 @@ dexp.genes = as.data.frame(res[!is.na(res$padj) &
                                  res$padj < 0.1 &
                                  res$log2FoldChange > log2(1.5), ])
 
+########################################################################################################
+
 write.table(
   x = as.data.frame(res),
   file = "genes-results.tsv",
@@ -26,6 +34,8 @@ write.table(
   quote = FALSE
 )
 
+########################################################################################################
+
 write.table(
   x = dexp.genes,
   file = "dexp-genes.tsv",
@@ -33,3 +43,5 @@ write.table(
   sep = "\t",
   quote = FALSE
 )
+
+########################################################################################################
